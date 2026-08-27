baa94f042dc16007c6ea7565181dd1276331dac7
darknecrocities <parejasarronkian@gmail.com>
2026-08-27 16:18:25 +0800

    chore(pwa): update service worker cache hash

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // DomoDomo PWA Service Worker (Auto-generated on build)
-const CACHE_NAME = 'domodomo-cache-2569c2c';
+const CACHE_NAME = 'domodomo-cache-ff5b38f';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
